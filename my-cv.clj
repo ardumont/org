1a08c63590b5842f23d1cae1970522c248fb3119
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-06-17 20:39:56 +0200

    My cv in clojure :D.

---
+++ my-cv.clj
@@ -0,0 +1,44 @@
+{:identity     {:name       "Dumont"
+                :first-name "Antoine R. Dumont"
+                :birth      "22/10/1982"
+                :location   "43 bis boulevard Jean Moulin"
+                :city       "Livry Gargan"
+                :country    "France"
+                :emails     ["antoine.romain.dumont[at]gmail.com"
+                             "eniotna.t[at]gmail.com"]}
+
+ :jobs         {:current     :sfeir
+                :sfeir       {:as     "software engineer/developer"
+                              :period "2011 - now"}
+                :sopra-group {:as     "software engineer"
+                              :period "2007-2011"}}
+
+ :experience   :contact-me-for-that
+
+ :technologies {:languages       ["clojure" "*sh" "c" "java" "php" "*sql" "xhtml" "etc..."]
+                :others          ["emacs-lisp" "common-lisp" "clojurescript" "objective-c" "arduino programming language"]
+                :tools           {:clojure ["emacs" "leiningen"]
+                                  :java    ["eclipse" "maven" "ant" ]
+                                  :linux   ["emacs" "make" "*sh" ]}
+                :nosql-databases ["hadoop"]
+                :sql-databases   ["mysql" "postgresql" "oracle" "mssql"]
+                :user-interface  {:java    ["GWT" "struts" "spring-mvc"]
+                                  :clojure ["noir" "compojure"]}}
+
+ :profiles     {:github            "https://github.com/ardumont"
+                :twitter           "http://twitter.com/@ardumont"
+                :gravatar          "http://en.gravatar.com/ardumont"
+                :linkedin          "http://www.linkedin.com/pub/antoine-romain-dumont/5/158/655"
+                :viadeo            "http://www.viadeo.com/fr/profile/antoine-romain.dumont"
+                :facebook          "https://www.facebook.com/profile.php?id=642018541"}
+
+ :projects     {:go-see-github     "https://github.com/ardumont"
+                :my-blog           "http://adumont.fr/blog"
+                :4-clojure         "http://www.4clojure.com/user/ardumont"
+                :project-euler     "http://projecteuler.net/profile/ardumont.png"
+                :clojure-paris-ug  "https://groups.google.com/forum/?fromgroups#!forum/clojure-paris-user-group"
+                :projectsion       "http://projectsion.sourceforge.net/"}
+
+ :specifics    ["father of 2" "geek and proud of it" "linuxian" "clojurian" "emacs user" "free software" "simplicity matters"]
+
+ :hobby        ["clojure" "coding" "reading" "running" "roller" "guitar" "drum"]}
