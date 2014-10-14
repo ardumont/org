9eaf99cac8416f074cb9b7ec97797072b8334bcf
Antoine R. Dumont <eniotna.t@gmail.com>
2014-10-14 15:18:13 +0200

    Refactor - Satisfy lint's warnings + improve names

--- euler/digit-fifth-powers.el
+++ euler/digit-fifth-powers.el
@@ -1,3 +1,7 @@
+;;; digit-fifth-powers.el --- Compute the sum of numbers to which their digits elevated to some exponent is themselves.
+;;; Commentary:
+;;; Code:
+
 (require 'dash)
 
 (defun number-to-digits (n)
@@ -11,15 +15,15 @@
     (--map (expt it e))
     (apply '+)))
 
-(defun compute-numbers-which-sum-of-digits-is-itself (l e)
-  "Filter numbers for which the sum of their digits to the exponent E is itself."
+(defun compute-numbers-which-sum-of-digits-elevated-to-exponent-is-itself (l e)
+  "Filter L for which the sum of their digits to the exponent E is itself."
   (--filter (= (sum-of-digits-elevated-to-exponent it e) it) l))
 
-(defun sum-of-digits-to-exponent (e)
-  "Compute the sum of the digits to the exponents e"
+(defun sum-of-numbers-which-sum-of-digits-elevated-to-exponent-is-itself (e)
+  "Compute the sum of the digits to the exponent E."
   (let ((upper-limit (* e (expt 9 e))))
     (--> (number-sequence 2 upper-limit)
-      (compute-numbers-which-sum-of-digits-is-itself it e)
+      (compute-numbers-which-sum-of-digits-elevated-to-exponent-is-itself it e)
       (apply '+ it))))
 
 (require 'ert)
@@ -27,14 +31,17 @@
 (require 'el-mock)
 
 (expectations
- (desc "compute-numbers-which-sum-of-digits-is-itself")
+ (desc "compute-numbers-which-sum-of-digits-elevated-to-exponent-is-itself")
  (expect '(1634 8208 9474)
-         (compute-numbers-which-sum-of-digits-is-itself '(1634 8208 9474 1000) 4))
+         (compute-numbers-which-sum-of-digits-elevated-to-exponent-is-itself '(1634 8208 9474 1000) 4))
  (expect '(4150 4151 54748 92727 93084 194979)
-         (compute-numbers-which-sum-of-digits-is-itself (number-sequence 2 295245) 5)))
+         (compute-numbers-which-sum-of-digits-elevated-to-exponent-is-itself (number-sequence 2 295245) 5)))
 
 (expectations
  (expect 19316
-         (sum-of-digits-to-exponent 4))
+         (sum-of-numbers-which-sum-of-digits-elevated-to-exponent-is-itself 4))
  (expect 443839
-         (sum-of-digits-to-exponent 5)))
+         (sum-of-numbers-which-sum-of-digits-elevated-to-exponent-is-itself 5)))
+
+(provide 'digit-fifth-powers)
+;;; digit-fifth-powers ends here
